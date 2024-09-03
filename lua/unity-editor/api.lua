6de3f53313937a4d1f78aa2dfb629aaebc629705
hat0uma <55551571+hat0uma@users.noreply.github.com>
2024-09-03 14:48:52 +0900

    feat: Add api `find_editor_instance`

--- lua/unity-editor/api.lua
+++ lua/unity-editor/api.lua
@@ -48,4 +48,51 @@ M.playmode_exit = unity_message_sender("playmode_exit", {})
 --- generate Visual Studio solution file
 M.generate_sln = unity_message_sender("generate_sln", {})
 
+--- load Unity EditorInstance.json
+---@param path string path to EditorInstance.json
+---@return UnityEditorInstance
+local function load_editor_instance_json(path)
+  local json_path = vim.fs.joinpath(path)
+  local f = assert(vim.uv.fs_open(json_path, "r", 438))
+  local stat = assert(vim.uv.fs_fstat(f))
+  local data = assert(vim.uv.fs_read(f, stat.size, 0))
+  local json = vim.fn.json_decode(data)
+  if type(json) ~= "table" then
+    error(string.format("%s decode failed.", json_path))
+  end
+  --  json must have process_id,version,app_path,app_contents_path
+  vim.validate({
+    process_id = { json.process_id, "number" },
+    version = { json.version, "string" },
+    app_path = { json.app_path, "string" },
+    app_contents_path = { json.app_contents_path, "string" },
+  })
+  return json
+end
+
+---@class UnityEditorInstance
+---@field process_id number
+---@field version string
+---@field app_path string
+---@field app_contents_path string
+
+--- Find Unity Editor Instance
+---@param bufnr integer
+---@return UnityEditorInstance?
+function M.find_editor_instance(bufnr)
+  local buf = vim.api.nvim_buf_get_name(bufnr)
+  local found = vim.fs.find("Library/EditorInstance.json", {
+    upward = true,
+    type = "file",
+    stop = vim.uv.os_homedir(),
+    path = vim.fs.dirname(buf),
+  })
+
+  if vim.tbl_isempty(found) then
+    return nil
+  end
+
+  return load_editor_instance_json(found[1])
+end
+
 return M
