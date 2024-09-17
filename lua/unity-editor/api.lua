14162195b46a614ddc245caafe8750d131fc62f8
hat0uma <55551571+hat0uma@users.noreply.github.com>
2024-09-18 01:29:20 +0900

    refactor: unify project request handling
    Refactored the code to unify the handling of project requests in Unity
    Editor. Introduced a new `request` function to reduce code duplication
    and improve maintainability.

--- lua/unity-editor/api.lua
+++ lua/unity-editor/api.lua
@@ -3,96 +3,71 @@ local M = {}
 
 local cl = require("unity-editor.ipc.client")
 
+--- Request Unity Editor to do something.
+---@param project_dir string? Unity project directory path
+---@param fn function(client: Client) function to request Unity Editor
+local function request(project_dir, fn)
+  -- if project_dir is not specified, find project root from current buffer
+  if not project_dir then
+    project_dir = M.find_project_root(vim.api.nvim_get_current_buf())
+  end
+
+  -- if project_dir is still not found, exit
+  if not project_dir then
+    vim.notify("Current buffer is not in Unity project", vim.log.levels.WARN)
+    return
+  end
+
+  -- if project is not open in Unity Editor, exit
+  if not M.is_project_open_in_unity(project_dir) then
+    vim.notify("This project is not open in Unity Editor", vim.log.levels.WARN)
+    return
+  end
+
+  local client = cl.get_project_client(project_dir)
+  fn(client)
+end
+
 --- refresh Unity Editor asset database
 --- this will compile scripts and refresh asset database
 --- It works like focus on Unity Editor or press Ctrl+R
 --- @param project_dir? string Unity project directory path
 function M.refresh(project_dir)
-  project_dir = project_dir or M.find_project_root(0)
-  if not project_dir then
-    vim.notify("This buffer is not in Unity project", vim.log.levels.WARN)
-    return
-  end
-
-  if not M.is_project_open_in_unity(project_dir) then
-    vim.notify("This project is not open in Unity Editor", vim.log.levels.WARN)
-    return
-  end
-
-  local client = cl.get_project_client(project_dir)
-  client:request_refresh()
+  request(project_dir, function(client)
+    client:request_refresh()
+  end)
 end
 
 --- request Unity Editor to play game
 ---@param project_dir? string Unity project directory path
 function M.playmode_enter(project_dir)
-  project_dir = project_dir or M.find_project_root(0)
-  if not project_dir then
-    vim.notify("This buffer is not in Unity project", vim.log.levels.WARN)
-    return
-  end
-
-  if not M.is_project_open_in_unity(project_dir) then
-    vim.notify("This project is not open in Unity Editor", vim.log.levels.WARN)
-    return
-  end
-
-  local client = cl.get_project_client(project_dir)
-  client:request_playmode_enter()
+  request(project_dir, function(client)
+    client:request_playmode_enter()
+  end)
 end
 
 --- request Unity Editor to stop game
 --- @param project_dir? string Unity project directory path
 function M.playmode_exit(project_dir)
-  project_dir = project_dir or M.find_project_root(0)
-  if not project_dir then
-    vim.notify("This buffer is not in Unity project", vim.log.levels.WARN)
-    return
-  end
-
-  if not M.is_project_open_in_unity(project_dir) then
-    vim.notify("This project is not open in Unity Editor", vim.log.levels.WARN)
-    return
-  end
-
-  local client = cl.get_project_client(project_dir)
-  client:request_playmode_exit()
+  request(project_dir, function(client)
+    client:request_playmode_exit()
+  end)
 end
 
 --- request Unity Editor to toggle play game
 ---@param project_dir? string Unity project directory path
 function M.playmode_toggle(project_dir)
-  project_dir = project_dir or M.find_project_root(0)
-  if not project_dir then
-    vim.notify("This buffer is not in Unity project", vim.log.levels.WARN)
-    return
-  end
-
-  if not M.is_project_open_in_unity(project_dir) then
-    vim.notify("This project is not open in Unity Editor", vim.log.levels.WARN)
-    return
-  end
-
-  local client = cl.get_project_client(project_dir)
-  client:request_playmode_toggle()
+  request(project_dir, function(client)
+    client:request_playmode_toggle()
+  end)
 end
 
 --- generate Visual Studio solution files
 ---@param project_dir? string Unity project directory path
 function M.generate_sln(project_dir)
-  project_dir = project_dir or M.find_project_root(0)
-  if not project_dir then
-    vim.notify("This buffer is not in Unity project", vim.log.levels.WARN)
-    return
-  end
-
-  if not M.is_project_open_in_unity(project_dir) then
-    vim.notify("This project is not open in Unity Editor", vim.log.levels.WARN)
-    return
-  end
-
-  local client = cl.get_project_client(project_dir)
-  client:request_generate_sln()
+  request(project_dir, function(client)
+    client:request_generate_sln()
+  end)
 end
 
 --- Check if the specified project is open in Unity Editor.
